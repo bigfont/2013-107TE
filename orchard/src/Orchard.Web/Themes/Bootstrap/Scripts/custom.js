68be673dfb5e4785a875a15ce96ab2c8ce2832e4
Shaun Luttin <shaunluttin@gmail.com>
2013-08-28 10:59:33 -0700

    Get viewport working in Windows ph 8 HTC.

--- orchard/src/Orchard.Web/Themes/Bootstrap/Scripts/custom.js
+++ orchard/src/Orchard.Web/Themes/Bootstrap/Scripts/custom.js
@@ -12,21 +12,4 @@
         $("body,html").animate({ scrollTop: 0 }, 2e3)
     })
 
-
-
-    if (navigator.userAgent.match(/(MSIE 10.0)|(MSIE)/i)) {
-        // handle windows phone 8 viewport
-        // see also http://timkadlec.com/2013/01/windows-phone-8-and-device-width/
-        var msViewportStyle, width;
-        msViewportStyle = document.createElement("style");
-        width = "device-width";
-        msViewportStyle.appendChild(
-            document.createTextNode(
-                "@-ms-viewport{width:" + width + "!important}"
-            )
-        );
-        document.getElementsByTagName("head")[0].
-            appendChild(msViewportStyle);
-    }
-
 });
